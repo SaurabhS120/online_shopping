e7d84b88e0be361fd8e47d1921b37991fad47af2
SaurabhS120 <Saurabh.sonar120@gmail.com>
2021-06-02 08:46:36 +0530

    Quantity color fix

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,7 +2,7 @@ import './CartItem.css';
 function CartItem(props){
     return(
         <div className="cart-item">
-            <p className="cart-item-name">{props.item.name+'\t- '+props.item.quantity+' nos.'}</p>
+            <p className="cart-item-name">{props.item.name}<span className='cart-item-quantity'>{'\t- '+props.item.quantity+' nos.'}</span></p>
             <div className="cart-item-delete" onClick={props.removeFromCart.bind(this,props.num)} >X</div>
         </div>
     );
